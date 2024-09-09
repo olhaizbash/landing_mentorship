1a330de71dff343a9446bb6f68fca47104620aaf
Olha Izbash <olgaizbash@gmail.com>
2024-09-09 18:13:38 +0200

    style changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const refs = {
   questions: document.querySelectorAll('.faq-list-item'),
+  openBtn: document.querySelector('[data-menu-open]'),
+  closeBtn: document.querySelector('[data-menu-close]'),
+  modal: document.querySelector('[data-menu]'),
+  menuLink: document.querySelectorAll('.menu-nav'),
 };
 
 refs.questions.forEach(question => {
@@ -7,3 +11,17 @@ refs.questions.forEach(question => {
     question.classList.toggle('active');
   });
 });
+
+refs.openBtn.addEventListener('click', toggleMenu);
+refs.closeBtn.addEventListener('click', toggleMenu);
+refs.menuLink.forEach(link => link.addEventListener('click', toggleMenu));
+
+function toggleMenu() {
+  refs.modal.classList.toggle('is-open');
+  const body = document.querySelector('body');
+  if (refs.modal.classList.contains('is-open')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
+}
